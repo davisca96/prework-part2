3ac8d08116a0758788a43a9f64860cd4e20abdb3
davisca6 <claire.a.davis@vanderbilt.edu>
2017-05-22 17:51:35 -0500

    added button

--- prework-week2/prework-week2/ViewController.swift
+++ prework-week2/prework-week2/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var PushButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Tapped(_ sender: Any) {
+        PushButton.titleLabel = "Changed!"
+    }
 
 }
 
